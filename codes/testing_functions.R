42318f18521c4685ab85b458bc22b431d4e826f5
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2024-12-21 10:59:40 -0500

    Create testing_functions.R

---
+++ codes/testing_functions.R
@@ -0,0 +1,111 @@
+#if(!require('Rcpp')){install.packages('Rcpp')}
+library(Rcpp)
+library(RcppArmadillo)
+# setwd('..')
+Rcpp::sourceCpp("functions/cpp_functions.cpp")
+
+
+TVD <- function(pi,pi.est){
+  return(0.5*sum(abs(pi-pi.est)))
+}
+
+X <- c(0,0,0,0,0)
+
+IIT_update_w(X,"sq",1)
+
+a_IIT_update(X,"sq",1,0)
+
+
+
+### Testing 
+processStrings(c("sq","min","min","min","min","sq","sq","sq","sq"))
+testassignment(5, c(1,.5,.3),c("sq","sq","sq"))
+
+
+#Maximo tenemos hasta dimension 30, pero es suficiente con eso
+vec_to_num(c(rep(1,30),1))
+vec_to_num(c(rep(0,31),1))
+
+
+######## Simple example
+#For p=2
+set.seed(123)
+p <- 8 #dimension
+total_iter <- 200000
+# (int p,int startsim,int endsim, int numiter,int iterswap, vec temp, const std::vector<std::string>& bal_function, bool bias_fix)
+ex1 <- PT_IIT_sim(p,startsim=1, endsim=1,numiter=total_iter,iterswap=1000,temp=c(1,0.3,0.05),bal_function=c("sq","sq","sq"), bias_fix = TRUE)
+ex2 <- PT_IIT_sim(p,startsim=1, endsim=1,numiter=total_iter,iterswap=1000,temp=c(1,0.3,0.05),bal_function=c("min","min","min"), bias_fix = TRUE)
+est <- ex1[[1]]
+est2 <- ex2[[1]]
+
+pi1 <- est/colSums(est)
+pi2 <- est2/colSums(est2)
+TVD(pi1,pi2)
+
+# Check replica swap acceptance
+ip1 <- ex1[[2]]
+ip2 <- ex2[[2]]
+
+swap_rate1 <- colSums(ip1[-1, ] != ip1[-nrow(ip1), ])
+swap_rate2 <- colSums(ip2[-1, ] != ip2[-nrow(ip2), ])
+
+swap_rate1/(nrow(ip1)-1)
+swap_rate2/(nrow(ip2)-1)
+
+(constant <- (1+exp(-1))^p)
+
+c(exp(0),exp(-1),exp(-1),exp(-2))/constant
+
+
+#With no swap
+
+
+
+loglik(c(0,0));loglik(c(1,0));loglik(c(0,1));loglik(c(1,1))
+
+exp(c(loglik(c(0,0)),loglik(c(1,0)),loglik(c(0,1)),loglik(c(1,1))))/sum(exp(c(loglik(c(0,0)),loglik(c(1,0)),loglik(c(0,1)),loglik(c(1,1)))))
+
+
+
+IIT_update_w(c(0,0),"sq",1)$Z
+IIT_update_w(c(1,0),"sq",1)$Z
+IIT_update_w(c(0,1),"sq",1)$Z
+IIT_update_w(c(1,1),"sq",1)$Z
+
+
+IIT_update_w(c(0,0),"min",1)$Z
+IIT_update_w(c(1,0),"min",1)$Z
+IIT_update_w(c(0,1),"min",1)$Z
+IIT_update_w(c(1,1),"min",1)$Z
+
+
+#For p=3
+p <- 3 #dimension
+# (int p,int startsim,int endsim, int numiter,int iterswap, vec temp, const std::vector<std::string>& bal_function, bool bias_fix)
+ex1 <- PT_IIT_sim(p,startsim=1, endsim=5,numiter=50000,iterswap=100,temp=c(1,0.7,0.5),bal_function=c("sq","sq","sq"), bias_fix = TRUE)
+est <- ex1[[1]]
+est/colSums(est)
+
+(constant <- (1+exp(-1))^p)
+exp(0)+ 3*exp(-1) + 3*exp(-2) + exp(-3)
+
+
+a <- est[,5]/sum(est[,5])
+
+IIT_update_w(rep(0,8),"sq",1)$Z
+IIT_update_w(rep(1,8),"sq",1)$Z
+
+IIT_update_w(c(1,0,0,1,0,1,0,1),"sq",1)$Z
+IIT_update_w(c(1,1,1,1,0,0,0,0),"sq",1)$Z
+
+
+
+
+#probability of the mode
+1/(1+exp(-1))^p
+
+est[1,1]/sum(est[,1])
+
+colSums(est)
+
+
