4e3050c42245019bf8105649f156d6f7fd616353
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2025-01-09 12:36:57 -0500

    deleting example of dimension 8

--- codes/low_d_multimodal_example.R
+++ codes/low_d_multimodal_example.R
@@ -36,67 +36,7 @@ TVD <- function(pi,pi.est){
 ##### Low-dimensional multimodal example #####
 Rcpp::sourceCpp("functions/cpp_functions.cpp")
 
-### computing normalizing constant
-
-### modes definition
-d=8
-mod1 <- rep(1,d)
-mod2 <- c(1,0,1,0,1,0,1,0)
-mod3 <- c(0,1,0,1,0,1,0,1)
-mod4 <- c(1,1,1,1,0,0,0,0)
-mod5 <- c(0,0,0,0,1,1,1,1)
-mod6 <- c(1,0,0,0,0,0,0,1)
-mod7 <- c(0,0,0,1,1,0,0,0)
-
-# ll_comp_2 <- function(X,theta=1){
-#   if(length(X)!=d){print('Error in dimension');return(-1);}
-#   total=0;
-#   for(m in 1:7){# loop for modes
-#     total = total+theta*sum(abs(X-eval(parse(text=paste0("mod",m)))))
-#   }
-# return(-total);
-# }
-
-
-# Testing function
-NumberToVector(2^d-1,d)
-NumberToVector(0,d)
-NumberToVector(7,d)
-##### corresponding states for the modes
-# vec_to_num(mod1)
-# vec_to_num(mod2)
-# vec_to_num(mod3)
-# vec_to_num(mod4)
-# vec_to_num(mod5)
-# vec_to_num(mod6)
-# vec_to_num(mod7)
-modes <- c(vec_to_num(mod1),  vec_to_num(mod2),  vec_to_num(mod3),  vec_to_num(mod4),  vec_to_num(mod5),  vec_to_num(mod6),  vec_to_num(mod7))
-#Computing log-likelihoods for all states
-loglik_vec <- rep(0,2^d)
-for(i in 0:(2^d -1)){
-  loglik_vec[i+1] <-  ll_comp(NumberToVector(i,d))
-}
-
-#Confirming the log-likelihood of the probabilities
-loglik_vec[modes+1]
-min(loglik_vec[modes+1])
-
-#Confirming those are the only modes
-modes[modes>=min(loglik_vec[modes+1])]
-
-#computing normalizing constant and comparing with theoretical constant
-sum(exp(loglik_vec))
-7*(1+exp(-1))^d
-
-
-##### Testing loglik function from CPP #####
-loglik(mod1)
-ll_comp(mod1)
-loglik(c(1,1,1,0,0,0,0,0))
-ll_comp(c(1,1,1,0,0,0,0,0))
-
 ##### simulations for multimodal example #####
-#For p=2
 p <- 16 #dimension
 theta <- 15
 # Modes definition
