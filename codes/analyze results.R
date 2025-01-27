3a75908a17bdf2bcb446ecc658956e9cbc09dae9
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2025-01-27 16:53:31 -0500

    add export of images into the overleaf github

--- codes/analyze results.R
+++ codes/analyze results.R
@@ -1,11 +1,11 @@
 rm(list=ls())
-### Process simulation results
+# Import libraries
 library(stringr)
 library(tidyverse)
 library(gridExtra)# For tables
 library(latex2exp) #For using latex
 
-
+### Process simulation results ###
 #Define seeds to filter
 filtered_seed <- 153
 #Create table with available files
@@ -33,8 +33,9 @@ iterations <- as.data.frame(matrix(NA,ncol=6)); colnames(iterations) <- c("alg",
 full_iter <- list()
 k <- 1
 
-# data_sum_full <- data_sum
+# filter specified seed
  data_sum <- data_sum |> filter(seed==filtered_seed)
+# Start creating datasets with information
 for(i in 1:nrow(data_sum)){
   data <- readRDS(file.path("results",data_sum[i,1]))
   tot_sim <- data_sum |> slice(i) |> pull(sim)
@@ -73,7 +74,7 @@ for(i in 1:nrow(data_sum)){
     temp <- temp |> select(alg,sim,everything())
     swap_rate <- rbind(swap_rate,temp)
   }
-  if(algorithm=="PT_A_IIT"){ 
+  if(algorithm=="A-IIT"){ 
     # Extract total iterations
     dims<- dim(data[["total_iter"]])
     full_iter[[k]] <- data[["total_iter"]]
@@ -93,12 +94,22 @@ mode_visit <- mode_visit |> filter(!is.na(alg))
 round_trip <- round_trip |> filter(!is.na(alg))
 swap_rate <- swap_rate |> filter(!is.na(alg))
 iterations <- iterations |> filter(!is.na(alg))
+
+##### Export plots and tables #####
+export_path <- "C:/Users/ralex/Documents/src/paper-adaptive-iit-latex/images/lowdim_ex"
 ##### Total Variation Distance #####
 
-tvd |>  filter(alg!='IIT') |> 
+tvd_plot <- tvd |>  filter(alg!='IIT') |> 
   ggplot(aes(x=alg,y=tvd,fill=alg)) +
-  geom_boxplot()+
-  labs(fill='Algortihm',x="",y="Total Variation Distance")
+  geom_boxplot(show.legend = FALSE)+
+  labs(fill='Algortihm',x="",y="Total Variation Distance")+
+  theme_minimal(base_size = 17)+
+  theme(legend.key.size = unit(1, 'cm'))
+tvd_plot
+
+jpeg(file.path(export_path,"tvd_lowdim.jpg"),width=800,height =400,pointsize = 30)
+tvd_plot
+dev.off()
 
 ##### First visit to modes #####
 col_names <- c("1","2","3","4","5","6","7")
@@ -111,8 +122,21 @@ mode_sum <- mode_visit |>
   mutate(total_modes = sum(c_across(all_of(col_names)) > 0)) |> 
   select(-all_of(col_names))
 
-##### Report on number of modes visited by each algorithm  
-mode_sum |> group_by(alg,total_modes) |> summarise(count=n())
+##### Report on number of modes visited by each algorithm 
+table_visited <- mode_sum |> rename(algorithm=alg) |> 
+  group_by(algorithm,total_modes) |> 
+  summarise(count=n()) |> ungroup() |> arrange(total_modes,desc(algorithm)) |> 
+  pivot_wider(id_cols =algorithm,
+              names_from = total_modes,
+              values_from = count,
+              values_fill=0)
+
+jpeg(file.path(export_path,"table_visited_modes.jpg"),,width=50*nrow(table_visited),height=40*nrow(table_visited),pointsize = 30)
+grid.arrange(tableGrob(table_visited))
+dev.off()
+
+
+
 
 ##### Report on number of iterations for the original replica to visit all modes (most of the times after a swap)
 iterations_to_explore <- mode_sum |> filter(alg!='IIT') |> 
@@ -123,10 +147,15 @@ iterations_to_explore <- mode_sum |> filter(alg!='IIT') |>
             mean=mean(last_visit),
             q3=quantile(last_visit,probs=0.75),
             max=max(last_visit))
+
+jpeg(file.path(export_path,"table_iterations.jpg"),width=150*nrow(iterations_to_explore),height=40*nrow(iterations_to_explore),pointsize = 30)
 grid.arrange(tableGrob(iterations_to_explore))
+dev.off()
+
+
 ##### Report on number of replica swaps needed to visit all modes
 # First algorithms do 2k iterations before trying a replica swap
-swaps_to_explore <- mode_sum |> filter(alg!='IIT',alg!='PT_A_IIT') |> 
+swaps_to_explore <- mode_sum |> filter(alg!='IIT',alg!="A-IIT") |> 
   group_by(alg) |> 
   summarise(min=min(last_visit)/2000,
             q1=quantile(last_visit,probs=0.25)/2000,
@@ -135,7 +164,7 @@ swaps_to_explore <- mode_sum |> filter(alg!='IIT',alg!='PT_A_IIT') |>
             q3=quantile(last_visit,probs=0.75)/2000,
             max=max(last_visit)/2000)
 
-temp <- mode_sum |> filter(alg=='PT_A_IIT') |>
+temp <- mode_sum |> filter(alg=="A-IIT") |>
   select(alg,sim,last_visit) |> 
   left_join(iterations,by=c("alg","sim")) |> 
   mutate(last_visit=last_visit/`1`) |> 
@@ -150,19 +179,29 @@ temp <- mode_sum |> filter(alg=='PT_A_IIT') |>
 
 
 swaps_to_explore <- rbind(swaps_to_explore,temp)
-
+jpeg(file.path(export_path,"table_swaps.jpg"),,width=140*nrow(swaps_to_explore),height=40*nrow(swaps_to_explore),pointsize = 30)
 grid.arrange(tableGrob(swaps_to_explore))
+dev.off()
+
 
 ##### Report on average swap rate
 swap_report <- swap_rate |> 
   group_by(alg) |>
   summarise(`1↔2`=mean(`1`),`2↔3`=mean(`2`),`3↔4`=mean(`3`))
 
+
+jpeg(file.path(export_path,"table_swap_rate.jpg"),,width=140*nrow(swap_report),height=40*nrow(swap_report),pointsize = 30)
 grid.arrange(tableGrob(swap_report))
+dev.off()
+
 
 ##### Report on average swap rate
 rt_report <- round_trip |> 
   group_by(alg) |>
   summarise(`R1`=mean(`1`),`R2`=mean(`2`),`R3`=mean(`3`),,`R4`=mean(`4`))
 
+jpeg(file.path(export_path,"table_roundtrip_rate.jpg"),,width=140*nrow(rt_report),height=40*nrow(rt_report),pointsize = 30)
 grid.arrange(tableGrob(rt_report))
+dev.off()
+
+
