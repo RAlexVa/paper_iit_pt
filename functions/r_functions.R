af8a7d5a6e4edf13accc750537c43bf6a65125be
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2025-02-24 16:23:12 -0500

    RT starts from min temperature

--- functions/r_functions.R
+++ functions/r_functions.R
@@ -87,14 +87,14 @@ PT_RT <- function(ip,total_swaps, total_sim){
   ip$sim <- rep(1:total_sim,each=total_swaps) #Add a column labeling each row with the simulation it corresponds
   ip <- ip |> select(sim,everything())# Re-order columns so simulation number is first column
   
-  #The round trip starts with the hottest temperature
+  #The round trip starts with temperature 0
   
   rt_count <- matrix(0,nrow = total_sim,ncol=ncol(ip)-1) # to track round trip rate
   
   for (c in 2:ncol(ip)){#Loop for each replica, ignoring first column that contains the number of simulation
     first_last <- (initial_ip[c-1]==max_temp)*max_temp + (initial_ip[c-1]==min_temp)*min_temp
     for(i in 1:nrow(ip)){#Loop for each iteration
-      if(i==1){# Restart the index process for each simulation
+      if(i==1){# Restart the index process for each replica
         last <- first_last
         start_rt <- FALSE
       }else if(ip[i,1]!=ip[i-1,1]){#If we changed the number of simulation
@@ -102,7 +102,7 @@ PT_RT <- function(ip,total_swaps, total_sim){
         start_rt <- FALSE
       }
       current <- ip[i,c]#Current iteration
-      if(current==max_temp & start_rt==FALSE){
+      if(current==min_temp & start_rt==FALSE){#Round trip starts in the MIN temperature
         # print(paste0("Start round trip for simulation ",ip[i,1]," at iteration ", i));
         start_rt <- TRUE;
         last <- current;
@@ -112,12 +112,13 @@ PT_RT <- function(ip,total_swaps, total_sim){
       
       if(start_rt){
         if(current==min_temp|current==max_temp){ #If it's one of the extreme temperatures
+#This is to count half round trips
           if(last!=current){# Se completo medio round trip
             # print(paste0("simulation ",ip[i,1]," iteration ",i," updating ",last," to ",current));
             rt_count[ip[i,1],c-1]=rt_count[ip[i,1],c-1]+0.5;#añadir medio round trip
             last <- current;#actualizar el current
-          }  
-        } 
+          }
+        }
       }
     }
   }
@@ -130,7 +131,10 @@ PT_RT <- function(ip,total_swaps, total_sim){
   # ip |> filter(V1==1|V1==4) |> select(sim,V1) |> filter(sim==9)
   # ip|> filter(sim==9) |> pull(V1)
   # ip |> filter(sim==9) |> pull(V3)
-  return(rt_count);
+# #Count half round trips  
+#   return(rt_count);
+#Count only full roundtrips
+  return(floor(rt_count))
 }
 
 
