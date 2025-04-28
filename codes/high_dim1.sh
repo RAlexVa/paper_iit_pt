631aff7b8a4cc42677f19dc4d87d97e9095b79aa
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2025-04-28 12:10:56 -0400

    some batch files

--- codes/high_dim1.sh
+++ codes/high_dim1.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=high_dim_job
 #SBATCH --output=output_%j.log
 #SBATCH --error=error_%j.log
-#SBATCH --time=00:15:00
+#SBATCH --time=06:00:00
 #SBATCH --mem=5G
 #SBATCH --cpus-per-task=10
 #SBATCH --mail-user=alexander.valencia@mail.utoronto.ca
@@ -11,6 +11,6 @@
 ####cd scratch/paper_iit_pt
 module load r/4.4.0
 
-PARAM="96"
+PARAM="97"
 Rscript --vanilla codes/highd_example.R $PARAM
 
--- codes/high_dim3.sh
+++ codes/high_dim3.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=high_dim_job
 #SBATCH --output=output_%j.log
 #SBATCH --error=error_%j.log
-#SBATCH --time=00:15:00
-#SBATCH --mem=5G
+#SBATCH --time=28:00:00
+#SBATCH --mem=8G
 #SBATCH --cpus-per-task=10
 #SBATCH --mail-user=alexander.valencia@mail.utoronto.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -11,6 +11,6 @@
 ####cd scratch/paper_iit_pt
 module load r/4.4.0
 
-PARAM="96"
+PARAM="99"
 Rscript --vanilla codes/highd_example.R $PARAM
 
