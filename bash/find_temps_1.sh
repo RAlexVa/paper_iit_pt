6156943d1c332f199e231d4f75051c9a5efba210
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2025-06-06 22:59:39 -0400

    batch for find_temps

---
+++ bash/find_temps_1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=find_temp_PT-IIT_5k
+#SBATCH --output=output_%a_%A.log
+#SBATCH --error=error_%a_%A.log
+#SBATCH --time=40:00:00
+#SBATCH --mem=16GB
+#SBATCH --cpus-per-task=32
+#SBATCH --mail-user=alexander.valencia@mail.utoronto.ca
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+#SBATCH --array=18-27
+
+module load r/4.4.0
+
+Rscript --vanilla codes/find_temps.R $SLURM_ARRAY_TASK_ID
---
+++ bash/find_temps_3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=find_temp_A-IIT
+#SBATCH --output=output_%a_%A.log
+#SBATCH --error=error_%a_%A.log
+#SBATCH --time=40:00:00
+#SBATCH --mem=16GB
+#SBATCH --cpus-per-task=10
+#SBATCH --mail-user=alexander.valencia@mail.utoronto.ca
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+#SBATCH --array=28-37
+
+module load r/4.4.0
+
+Rscript --vanilla codes/find_temps.R $SLURM_ARRAY_TASK_ID
