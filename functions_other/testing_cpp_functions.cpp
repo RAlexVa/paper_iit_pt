98c714aba024eda6c2fc7e4784998aff07f2716a
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2025-06-10 09:36:55 -0400

    adding parallel code to testing script

--- functions_other/testing_cpp_functions.cpp
+++ functions_other/testing_cpp_functions.cpp
@@ -7,6 +7,13 @@
 // [[Rcpp::depends(RcppArmadillo)]]
 using namespace Rcpp;
 using namespace arma;
+// [[Rcpp::depends(RcppParallel)]]
+#include <RcppParallel.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h>
+using namespace RcppParallel;
+
+
 
 ////////// Other functions //////////
 
@@ -184,4 +191,285 @@ NumericMatrix check_x(int p, int T){
  
   
 }
+
+
+
+/////////////////////////////////////////////////
+//// benchmark performance of updates
+/////////////////////////////////////////////////
+double bf_sq(double x){
+  return x/2;
+}
+double bf_min(double x){
+  double result;
+  if(x<0){
+    result = x;
+  }else{
+    result = 0;
+  }
+  return result;
+}
+
+//This function we don't export to R because it generates errors
+double invoke(double x, double (*func)(double)) {
+  return func(x);
+}
+
+double apply_bal_func(double x,const int chosen){
+  if (chosen == 2) {
+    return invoke(x, &bf_sq);
+  } else if (chosen == 1) {
+    return invoke(x, &bf_min);
+  } else {
+    Rcpp::Rcout <<"The balancing function does exist!" << std::endl;
+    return -1000; // Default return for unknown operation
+  }
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+int L1_distance(const arma::Col<double>& v1, const arma::Col<double>& v2) {
+  // Check if the vectors have the same size
+  if (v1.n_elem != v2.n_elem) {
+    Rcpp::Rcout <<"Vectors are not the same size" << std::endl;
+    return -1;
+  }else{
+    int dist=sum(abs(v1-v2));
+    return dist;
+  }
+}
+
+
+// [[Rcpp::export]]
+double loglik(const arma::Col<double>& X,const arma::Mat<double>& M, const double& theta){
+  double loglik_computed;
+  if(M.n_cols==2){
+    //Define vectors to represent each mode
+    arma::Col<double> mod1=M.col(0);
+    arma::Col<double> mod2=M.col(1);
+    // Compute distances to each mode  
+    double dif1=L1_distance(X,mod1);
+    double dif2=L1_distance(X,mod2);
+    
+    if(dif2<=dif1){
+      loglik_computed = -dif2*theta + log1p(exp((dif2-dif1)*theta));
+    }
+    if(dif2>dif1){
+      loglik_computed = -dif1*theta + log1p(exp((dif1-dif2)*theta));
+    }
+    return(loglik_computed);
+    
+  }else{
+    Rcpp::Rcout << "Error matrix number of columns isn't 2" << std::endl;
+    Rcpp::Rcout <<"M: " <<M<< std::endl;
+    return(-10000);
+  }
+  
+  
+  
+}
+
+
+
+///// Here include the for in the parallelization AND make it reproducible /////
+//// Definition of worker
+struct Parallel_replica_update_rep : public Worker
+{
+  //// Data
+  RMatrix<double> X_n_matrix;//State matrix
+  const RMatrix<double> Q_matrix; //Matrix with modes
+  const int bal_func; //Specified balancing function
+  const RVector<double> temperatures;//Vector of temperatures
+  const double theta; //Parameter for 
+  RMatrix<double> output;
+  const std::size_t dim_p; // dimension of the problem (and length of X columns)
+  const std::size_t num_temp; // dimension of the problem (and length of X columns)
+  const int num_iter;//Number of iterations in the for loop
+  std::vector<std::mt19937_64>& rngs;//Vector of RNGs
+  //// Functions to use from outside
+  arma::Col<double> IIT_update_p(arma::Col<double> X,
+                                 const arma::Mat<double>& M,
+                                 const int chosen_bf,
+                                 const double& temperature,
+                                 const double& theta,
+                                 std::mt19937_64& rng);
+  
+  ////  Functions to transform data
+  arma::Mat<double> convert_X(){
+    RMatrix<double> tmp_matrix = X_n_matrix;
+    arma::Mat<double> MAT(tmp_matrix.begin(), dim_p, num_temp, false,true);
+    return MAT;
+  }
+  arma::Mat<double> convert_Q(){
+    RMatrix<double> tmp_matrix = Q_matrix;
+    arma::Mat<double> MAT(tmp_matrix.begin(), dim_p, 2, false,true);
+    return MAT;
+  }
+  arma::Col<double> convert_temps(){
+    RVector<double> tmp_vec = temperatures;
+    arma::Col<double> vec(tmp_vec.begin(),num_temp,false,true);
+    return vec;
+  }
+  
+  //// Main constructor
+  Parallel_replica_update_rep(
+    NumericMatrix X_n_matrix_in,
+    const NumericMatrix Q_matrix_in,
+    const int bal_func_in,
+    const NumericVector temperature_in,
+    const double theta_in,
+    NumericMatrix output_in,
+    const std::size_t dim_p_in,
+    const std::size_t num_temp_in,
+    const int num_iter_in,
+    std::vector<std::mt19937_64>& rngs_in)://This binds the class members (defined above) to the constructor arguments
+    X_n_matrix(X_n_matrix_in),
+    Q_matrix(Q_matrix_in),
+    bal_func(bal_func_in),
+    temperatures(temperature_in),
+    theta(theta_in),
+    output(output_in),
+    dim_p(dim_p_in),
+    num_temp(num_temp_in),
+    num_iter(num_iter_in),
+    rngs(rngs_in)
+  {}
+  
+  //// Operator
+  void operator()(std::size_t begin, std::size_t end){
+    //First transform all the inputs
+    arma::Mat<double> X = convert_X();
+    arma::Mat<double> Q_matrix = convert_Q();
+    arma::Col<double> temperatures = convert_temps();
+    int dim_p_int = static_cast<int>(dim_p);
+    
+    for(std::size_t r=begin; r<end;r++){ // For for each replica
+      double current_temp = temperatures(r);
+      arma::Col<double> selected_X=X.col(r); // Get the status of the replica
+      arma::Col<double> temporal_X(dim_p);
+      std::mt19937_64 selected_rng=rngs[r];
+      for(int k=0;k<num_iter;k++){//Repeat for as many iterations
+        temporal_X=IIT_update_p(selected_X,
+                                Q_matrix,
+                                bal_func,
+                                current_temp,
+                                theta,
+                                selected_rng);
+        selected_X=temporal_X;//Assign new state for the loop
+      }
+      // After num_iter updates, export the resulting file
+      for(int i=0;i<dim_p_int;i++){
+        output(i,r)=temporal_X[i];
+      }//End loop for the export 
+    }// End loop for each replica
+    
+  }//End operator
+};// End of worker
+
+// Full definition of update function used inside the worker
+arma::Col<double> Parallel_replica_update_rep::IIT_update_p(arma::Col<double> X,
+                                                            const arma::Mat<double>& M,
+                                                            const int chosen_bf,
+                                                            const double& temperature,
+                                                            const double& theta,
+                                                            std::mt19937_64& rng){
+  int total_neighbors = X.n_rows; // total number of neighbors is p spacial
+  vec probs(total_neighbors, fill::zeros); //vector of probabilities
+  double logpi_current = loglik(X,M,theta);  // Compute likelihood of current state
+  
+  ////// Compute weight for all neighbors
+  double temporal=0;
+  vec newX;
+  for(int j=0; j<total_neighbors;j++){
+    newX = X;
+    newX.row(j) = 1-X.row(j);
+    temporal=loglik(newX,M,theta)-logpi_current;
+    probs(j)=apply_bal_func(temporal*temperature, chosen_bf);//Apply balancing function to log probability times temperature
+  }
+  //////Choose the next neighbor
+  // vec u(total_neighbors,fill::randu);
+  vec u(total_neighbors);
+  std::uniform_real_distribution<double> dist(0.0, 1.0);
+  for(int j=0; j<total_neighbors; j++) {
+    u(j) = dist(rng);
+  }
+  vec probs_choose = log(-log(u)) - probs;
+  int neigh_pos = (std::min_element(probs_choose.begin(), probs_choose.end()))-probs_choose.begin();
+  X.row(neigh_pos) = 1-X.row(neigh_pos); //modify the coordinate of the chosen neighbor
+  return X;
+}
+
+
+
+// Function to initialize the rng according to the seed
+std::vector<std::mt19937_64> initialize_rngs(int n, int base_seed) {
+  std::vector<std::mt19937_64> rngs(n);
+  for(int i = 0; i < n; i++) {
+    rngs[i].seed(base_seed + i);  // Unique seed for each RNG
+  }
+  return rngs;
+}
+//Function to run simulations
+// [[Rcpp::export]]
+NumericMatrix PT_IIT_parallel_sim_rep(int p, int num_iter, arma::Col<double> original_temperatures, const int chosen_bal_func, double theta, int base_seed){
+  int T=original_temperatures.n_rows;
+  const std::size_t dim_p = static_cast<size_t>(p);
+  const std::size_t num_temps = static_cast<size_t>(T);
+  const std::size_t begin = static_cast <size_t> (0);
+  const std::size_t end = static_cast <size_t> (T); 
+  // Change String of balancing function into int
+  int chosen_bal_func;
+  if(bal_func=="sq"){
+    chosen_bal_func=2;
+  }else if(bal_func=="min"){
+    chosen_bal_func=1;
+  }else{
+    Rcpp::Rcout << "Incorrect definition of Balancing function.\n Defaulting to sq " << std::endl;
+    chosen_bal_func=2;
+  }
+  //// Define two modes
+  NumericMatrix Q_matrix(p,2);
+  for(int i=0;i<p;i++){
+    if(i%2==0){Q_matrix(i,1)=1;}
+    if(i%2==1){Q_matrix(i,0)=1;}
+  }
+  double ppp=randu();
+  arma::Mat<double> original_X(p,T);
+  original_X=initializeRandom(p,T,ppp);//Randomly initialize the state of each replica.
+  std::vector<std::mt19937_64> defined_rngs=initialize_rngs(T,base_seed); // Initialize RNGs
+  //After initialization of vectors and matrices we transform them to Nuneric variables
+  NumericMatrix X=Rcpp::wrap(original_X);
+  NumericVector temperatures=Rcpp::wrap(original_temperatures);
+  // Now we apply the parallelized loop
+  
+  NumericMatrix output(p,T);//Declare output
+  Parallel_replica_update_rep par_rep_update_rep(//Declare Worker
+      X,Q_matrix,
+      chosen_bal_func,
+      temperatures,
+      theta,
+      output,
+      dim_p,
+      num_temps,
+      num_iter,
+      defined_rngs);// Initialize the worker
+  
+  parallelFor(begin,end,par_rep_update_rep);//Perform the for loop
+  
+  return output;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
