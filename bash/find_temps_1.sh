e7a4bda37c22dce1dfdf960c765c52e6e54e0431
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2025-06-17 21:26:55 -0400

    updating bash

--- bash/find_temps_1.sh
+++ bash/find_temps_1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=find_temp_PT-IIT_5k
+#SBATCH --job-name=find_temp_PT-IIT_1k
 #SBATCH --output=output_%a_%A.log
 #SBATCH --error=error_%a_%A.log
 #SBATCH --time=20:00:00
-#SBATCH --mem=16GB
+#SBATCH --mem=4GB
 #SBATCH --cpus-per-task=20
 #SBATCH --mail-user=alexander.valencia@mail.utoronto.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-#SBATCH --array=18-27
+#SBATCH --array=67-68
 
 module load r/4.4.0
 
--- bash/highd_array_1.sh
+++ bash/highd_array_1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=hdim_PT-IIT_p800
+#SBATCH --job-name=hdim_PT-IIT_1k
 #SBATCH --output=output_%a_%A.log
 #SBATCH --error=error_%a_%A.log
-#SBATCH --time=10:00:00
-#SBATCH --mem=8GB
-#SBATCH --cpus-per-task=10
+#SBATCH --time=15:00:00
+#SBATCH --mem=4GB
+#SBATCH --cpus-per-task=20
 #SBATCH --mail-user=alexander.valencia@mail.utoronto.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-#SBATCH --array=302-311
+#SBATCH --array=372-386
 
 ####cd scratch/paper_iit_pt
 module load r/4.4.0
