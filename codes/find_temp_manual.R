be887b88cae23bb85cd9c30d50468c55ad119a66
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2025-06-25 23:40:40 -0400

    Some modifications for multimodal
    include number of modes and direction of search, reduce the size of modification (deleting the times 3), increased the lower threshold

--- codes/find_temp_manual.R
+++ codes/find_temp_manual.R
@@ -24,20 +24,10 @@ find_temp_highd <- function(list_ids){
     ##### Read file for parameters #####
     parameters <- as.data.frame(read_csv("inputs/find_temps.csv", col_types = cols()))
     
-    #Check how many models we're doing
-    tot_models <- unique(parameters|> filter(id %in% list_ids) |> pull(model))
-    if(length(tot_models)==1){only_1_model <- TRUE;}else{only_1_model <- FALSE}
-    # In case only 1 model is chosen, we only read 1 model for all the IDs    
-    if(only_1_model){
-      print("Reading one set of C++ functions")
-      print(paste0("Model = ",tot_models))
-      if(tot_models=="bimodal"){
-        # Rcpp::sourceCpp("functions/cpp_functions_highdim_2.cpp");
-        Rcpp::sourceCpp("functions/highdim_2_parallel.cpp");}
-    }
+    Rcpp::sourceCpp("functions/highdim_2_parallel.cpp");
+
     #Start process for algorithms
     for(id_chosen in list_ids){
-      counter_negative_temp <- 0;
       sim_chosen <- parameters |> filter(id==id_chosen)
       if(nrow(sim_chosen)!=1){print(paste0("Error: id ",id_chosen," doesn't exist or there's more than one")); next;}
       #In case there are more than 1 model, I need to re-read functions depending on model
@@ -46,12 +36,16 @@ find_temp_highd <- function(list_ids){
       total_simulations <- sim_chosen$tot_sim
       p <- sim_chosen$p
       num_temps <- sim_chosen$num_temp
+      num_modes <- sim_chosen$num_modes
+      search_direction <- sim_chosen$direction
       temp_ini<- sim_chosen$t1
       temperatures <- rep(0,num_temps)
       temperatures[1] <- temp_ini
       rho_vector <- rep(-5,num_temps-1)
       for(t in 2:num_temps){
-        temperatures[t]=temperatures[t-1]/(1+exp(rho_vector[t-1]))
+        if(sign(search_direction)<0){temperatures[t]=temperatures[t-1]/(1+exp(rho_vector[t-1]))}
+        if(sign(search_direction)>0){temperatures[t]=temperatures[t-1]*(1+exp(rho_vector[t-1]))}
+        
       }
       original_temperatures <- temperatures;
       bal_f <- as.character(sim_chosen$bf)
@@ -72,11 +66,13 @@ find_temp_highd <- function(list_ids){
                    paste0("ID: ",id_chosen),
                    paste0("Algorithm: ",alg),
                    paste0("Problem: ",sim_chosen$model),
+                   paste0("Num Modes: ",num_modes),
                    paste0("Number of Replicas: ",length(temperatures)),
                    paste0("Total simulations: ",total_simulations),
                    paste0("Burn-in iterations: ",burnin_iter),
                    paste0("Total iterations: ",total_iter),
                    paste0("Temperatures: ",paste(temperatures,collapse=',')),
+                   paste0("Search direction: ",search_direction),
                    paste0("Balancing function: ",paste(bal_f,collapse = ',')),
                    paste0("Try swaps:",sample_inter_swap),
                    paste0("Samples in-between swaps: ",sample_inter_swap),
@@ -94,21 +90,22 @@ find_temp_highd <- function(list_ids){
     if(alg=="PT_IIT_Z"){
       # Using Z factor bias correction
       # PT_IIT_sim(int p,int startsim,int endsim, int numiter, int iterswap,int burn_in, vec temp, int bal_func, bool bias_fix,const std::string& filename,int num_states_visited,const std::vector<int>& starting_coord, double theta)
-      output <- PT_IIT_sim(p,1,total_simulations,total_iter,sample_inter_swap,burnin_iter,temperatures,bal_f,TRUE,"",0,0,3)
+      output <- PT_IIT_sim(p,1,total_simulations,total_iter,sample_inter_swap,burnin_iter,temperatures,bal_f,TRUE,"",0,0,3,num_modes)
     }
     if(alg=="PT_A_IIT"){
       # Using A-IIT in each replica
       # PT_a_IIT_sim(int p,int startsim,int endsim, int total_swaps,int sample_inter_swap,int burn_in, vec temp, const int bal_func,const std::string& filename,int num_states_visited,const std::vector<int>& starting_coord, double decreasing_constant,std::string reduc_model, double theta)
-      output <- PT_a_IIT_sim(p,1,total_simulations,total_swap,sample_inter_swap,burnin_iter,temperatures,bal_f,"",0,0,0,"never",3)
+      output <- PT_a_IIT_sim(p,1,total_simulations,total_swap,sample_inter_swap,burnin_iter,temperatures,bal_f,"",0,0,0,"never",3,num_modes)
     }
 
     
     swap_rate <- as.data.frame(output[["swap_rate"]])
     summary_sr <- colSums(swap_rate)/nrow(swap_rate) #Get avg. swap rate
     distance_sr <- summary_sr-target_sr;
-    new_rho <- rho_vector+3*distance_sr;
+    # new_rho <- rho_vector+3*distance_sr;
+    new_rho <- rho_vector+distance_sr;
     for(i in 1:length(summary_sr)){
-      if(summary_sr[i]>0.2335 && summary_sr[i]<0.27){
+      if(summary_sr[i]>0.234 && summary_sr[i]<0.27){
         #Dont change the rho_factor
         check_convergence[i] <- TRUE
       }else{
@@ -117,7 +114,9 @@ find_temp_highd <- function(list_ids){
     }
     #Update temperatures
     for(t in 2:num_temps){
-      temperatures[t]=temperatures[t-1]/(1+exp(rho_vector[t-1]))
+      if(sign(search_direction)<0){temperatures[t]=temperatures[t-1]/(1+exp(rho_vector[t-1]))}
+      if(sign(search_direction)>0){temperatures[t]=temperatures[t-1]*(1+exp(rho_vector[t-1]))}
+      
     }
     
  print(paste0("Rho Vector ",paste0(rho_vector,collapse=",")));
