af4f300b05fc88e0fb04f50ed9962fab29bacd0c
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2025-06-25 23:57:23 -0400

    bash files

--- bash/find_temps_2.sh
+++ bash/find_temps_2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=find_t_PT-IIT_1k
+#SBATCH --job-name=find_t_A-IIT_1k
 #SBATCH --output=output_%a_%A.log
 #SBATCH --error=error_%a_%A.log
 #SBATCH --time=20:00:00
 #SBATCH --mem=5GB
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=10
 #SBATCH --mail-user=alexander.valencia@mail.utoronto.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-#SBATCH --array=83-85,89-91
+#SBATCH --array=80-82,86-88
 
 module load r/4.4.0
 
