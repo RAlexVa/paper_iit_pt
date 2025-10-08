ccb169848bdb3c6b73aab345a611799e006895d2
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2025-10-08 18:37:11 -0400

    bash to run find temp for Gset

--- bash/find_temps_1.sh
+++ bash/find_temps_1.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=find_t_PT-IIT_1k
+#SBATCH --job-name=find_t_A-IIT_G
 #SBATCH --output=output_%a_%A.log
 #SBATCH --error=error_%a_%A.log
-#SBATCH --time=20:00:00
-#SBATCH --mem=5GB
-#SBATCH --cpus-per-task=20
+#SBATCH --time=48:00:00
+#SBATCH --mem=16GB
+#SBATCH --cpus-per-task=10
 #SBATCH --mail-user=alexander.valencia@mail.utoronto.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-#SBATCH --array=89-91,83-85
+#SBATCH --array=167,169
 
-module load r/4.4.0
+module load r/4.5.0
 
 Rscript --vanilla codes/find_temp_manual.R $SLURM_ARRAY_TASK_ID
--- bash/find_temps_2.sh
+++ bash/find_temps_2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=find_t_A-IIT_1k
+#SBATCH --job-name=find_t_PT-IIT_G
 #SBATCH --output=output_%a_%A.log
 #SBATCH --error=error_%a_%A.log
-#SBATCH --time=20:00:00
-#SBATCH --mem=5GB
+#SBATCH --time=48:00:00
+#SBATCH --mem=16GB
 #SBATCH --cpus-per-task=10
 #SBATCH --mail-user=alexander.valencia@mail.utoronto.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-#SBATCH --array=86-88,80-82
+#SBATCH --array=168,170
 
-module load r/4.4.0
+module load r/4.5.0
 
 Rscript --vanilla codes/find_temp_manual.R $SLURM_ARRAY_TASK_ID
