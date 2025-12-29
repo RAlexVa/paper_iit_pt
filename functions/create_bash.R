2a9ed74892739dd9f055dc742cbdc7257816a96a
Alex Valencia <100150677+RAlexVa@users.noreply.github.com>
2025-12-29 15:57:16 -0500

    Update create_bash.R
    adding option to define the specific number of simulations to use

--- functions/create_bash.R
+++ functions/create_bash.R
@@ -2,7 +2,9 @@ create_sh_files <- function(numbers,
                             prefix = "high",
                             hours=48,
                             cpus=20,
-                            num_sims=100,
+                            sim_ini=1,
+                            sim_fin=100,
+                            memory_gb=16,
                             output_dir = ".",
                             name_file="default") {
   # Ensure output directory exists
@@ -18,12 +20,12 @@ create_sh_files <- function(numbers,
 #SBATCH --output=output_%a_%A.log
 #SBATCH --error=error_%a_%A.log
 #SBATCH --time=HOURS:00:00
-#SBATCH --mem=5GB
+#SBATCH --mem=MEMORYGB
 #SBATCH --cpus-per-task=CPUS
 #SBATCH --mail-user=alexander.valencia@mail.utoronto.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-#SBATCH --array=1-NUMSIMS
+#SBATCH --array=SIMINI-SIMFIN
 
 module load r/4.5.0
 
@@ -35,8 +37,10 @@ Rscript --vanilla codes/highd_seeded.R $FIXED_INPUT $SLURM_ARRAY_TASK_ID
     content <- gsub("PLACEHOLDER", numbers[i], content)
     content <- gsub("HOURS", hours, content)
     content <- gsub("CPUS", cpus, content)
-    content <- gsub("NUMSIMS", num_sims, content)
+    content <- gsub("SIMINI", sim_ini, content)
+    content <- gsub("SIMFIN", sim_fin, content)
     content <- gsub("NAMEFILE", name_file, content)
+    content <- gsub("MEMORY", memory_gb, content)
     
     # # Write the file with UNIX line endings
     # writeLines(content, filename, sep = "\n")
@@ -53,20 +57,59 @@ Rscript --vanilla codes/highd_seeded.R $FIXED_INPUT $SLURM_ARRAY_TASK_ID
 }
 
 # Usage with your example vector
-numbers <- c(884:895)
+numbers <- c(1058:1061)
 create_sh_files(numbers,
-                prefix = "higher",
+                prefix = "spa1",
                 hours=100,
                 cpus=20,
-                num_sims=20,
+                sim_ini=1,
+                sim_fin=20,
+                memory_gb=25,
                 output_dir = "bash",
-                name_file="3_5_7k_6mod_13temp")
+                name_file="spaced_500")
+numbers <- c(1062:1065)
+create_sh_files(numbers,
+                prefix = "spa2",
+                hours=100,
+                cpus=20,
+                sim_ini=21,
+                sim_fin=40,
+                memory_gb=25,
+                output_dir = "bash",
+                name_file="spaced_500")
 
-numbers <- c(896:907)
+
+
+
+numbers <- c(1052:1057)
 create_sh_files(numbers,
                 prefix = "high",
+                hours=20,
+                cpus=20,
+                sim_ini=1,
+                sim_fin=25,
+                memory_gb=2,
+                output_dir = "bash",
+                name_file="1k_13temp_diftemp")
+
+numbers <- c(1012:1023)
+create_sh_files(numbers,
+                prefix = "less_rf",
+                hours=8,
+                cpus=20,
+                sim_ini=1,
+                sim_fin=25,
+                memory_gb=5,
+                output_dir = "bash",
+                name_file="6mod_13t_less_rf")
+
+numbers <- c(978:1007)
+create_sh_files(numbers,
+                prefix = "more_rf",
                 hours=100,
                 cpus=20,
-                num_sims=20,
+                sim_ini=1,
+                sim_fin=25,
+                memory_gb=16,
                 output_dir = "bash",
-                name_file="3_5_7k_6mod_7temp")
+                name_file="6mod_13t_less_rf")
